c0ebc5238ae75c45b263eb244b81888fcc1ee9cd
lipadsoft-Engineer <pierreclassique391@gmail.com>
2025-05-13 13:59:13 +0300

    Add blog post model

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Blogpost(models.Model):
+    class Publish_status(models.TextChoice):
+        DRAFT = 'draft', 'Draft'
+        PUBLISHED = 'published', 'Published'
+        ARCHIVED = 'archived', 'Archived'
+        
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=20,choices=Publish_status.choices,
+    default=Publish_status.DRAFT)   
+    excerpt = models.TextField(max_length=200, blank
+    =True) 
+
