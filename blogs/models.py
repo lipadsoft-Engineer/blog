1d06199a41e62cfa31721df14f3b157df4459c67
lipadsoft-Engineer <pierreclassique391@gmail.com>
2025-05-13 19:56:30 +0300

    Add many to many relations for blog model

--- blogs/models.py
+++ blogs/models.py
@@ -22,6 +22,11 @@ class Blogpost(models.Model):
 
     def __str__(self):
         return self.title 
+    
+    class Category(models.Model):
+        name = models.CharField(max_length=100)
+        slug = models.SlugField(max_length=100, unique=True)
 
-        
+        def __str__(self):
+            return self.name        
 
