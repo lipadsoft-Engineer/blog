0d5cf2726c1628afe2e46d704ed047fa4959763d
lipadsoft-Engineer <pierreclassique391@gmail.com>
2025-05-14 22:16:00 +0300

    Register the blogs app

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blogs.apps.BlogsConfig',
     'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
