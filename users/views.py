8060185ebba5580808fd252038dece2f01b71a7c
lipadsoft-Engineer <pierreclassique391@gmail.com>
2025-05-15 21:01:21 +0300

    Create a dynamic bonjour fn

--- users/views.py
+++ users/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 def users(request):
     return HttpResponse("Hello world!")
 
-def bonjour(request):
-    return HttpResponse("Bonjour, peter")
+def bonjour(request, name):
+    return HttpResponse(f"Bonjour, {name}")
