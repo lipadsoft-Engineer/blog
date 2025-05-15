f9b5980f35a1cc0d394154210a10a5db27884cd1
lipadsoft-Engineer <pierreclassique391@gmail.com>
2025-05-15 20:35:33 +0300

    Add url path for peter

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("users", views.users, name="users"),
+    path("", views.users, name="users"),
+    path("peter/", views.bonjour, name="peter"),
 ]
