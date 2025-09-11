2c093ae955c6c86a5106d0ec4d7cc712c30392f5
lipadsoft-Engineer <pierreclassique391@gmail.com>
2025-09-11 23:27:56 +0300

    Remove unnecessary positional args

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 
+tasks = [ "sleep", "pray", "eat"]
+
 # Create your views here.
 def index(request):
-    return render(request, "tasks/index.html")
+    return render(request, "tasks/index.html", {
+        "tasks": tasks,
+    })
 
 def ask(request, name):
     return render(request, "tasks/ask.html", {
