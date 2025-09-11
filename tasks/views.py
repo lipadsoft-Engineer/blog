c011aec56a1fff1b4b024d343910524dd3f74dda
lipadsoft-Engineer <pierreclassique391@gmail.com>
2025-09-11 21:41:05 +0300

    Create task app

--- tasks/views.py
+++ tasks/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "tasks/index.html")
+
+def ask(request):
+    return render(request, "ask.html")
