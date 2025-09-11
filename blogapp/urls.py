26bb3bc8424c042bae4280adc88eb0ded41b7b81
lipadsoft-Engineer <pierreclassique391@gmail.com>
2025-09-11 21:57:42 +0300

    Remove greet

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -18,9 +18,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("tasks/", include("tasks.urls")),
     path("", include("blogs.urls")),
-    path("greet/", include("blogs.urls")),
+    path("tasks/", include("tasks.urls")),
     path("blogs/", include("blogs.urls")),
     path("users/", include("users.urls")),
     path('admin/', admin.site.urls),
