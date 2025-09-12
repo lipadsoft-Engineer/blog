ee544aeaa757d8b9db4d5110b8b381b94a0d9cef
lipadsoft-Engineer <pierreclassique391@gmail.com>
2025-09-12 08:55:16 +0300

    Add new year view

--- users/views.py
+++ users/views.py
@@ -1,7 +1,16 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+import datetime
+
+now = datetime.datetime.now()
 
 def users(request):
     return HttpResponse("Hello world!")
 
 def bonjour(request, name):
     return HttpResponse(f"Bonjour, {name.capitalize()}!")
+
+def is_new_year(request):
+    return render(request, "users/index.html", {
+        "date": now
+    })
