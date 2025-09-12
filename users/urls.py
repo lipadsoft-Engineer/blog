e6eee6088e85c55ee846d73f5cb499df8d57c03b
lipadsoft-Engineer <pierreclassique391@gmail.com>
2025-09-12 09:06:54 +0300

    Add url for new year check

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.users, name="users"),
     path("<str:name>", views.bonjour, name="bonjour"),
+    path("new year/", views.is_new_year, name="new_year")
 ]
