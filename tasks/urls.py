7cc8941acfe73d31f914a4a4335a6136290c5804
lipadsoft-Engineer <pierreclassique391@gmail.com>
2025-09-15 21:16:31 +0300

    Add url for add-task

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path(f"<str:name>", views.ask, name="ask" )
+    path(f"<str:name>", views.ask, name="ask" ),
+    path("add", views.add_task, name='add_task')
 ]
