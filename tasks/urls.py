f40988059751acf5fb9637733375a81da224bea7
lipadsoft-Engineer <pierreclassique391@gmail.com>
2025-09-15 21:52:27 +0300

    Rename index url

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.index, name="task_index"),
     path("add", views.add_task, name='add_task'),
     path(f"<str:name>", views.ask, name="ask" ),
 ]
