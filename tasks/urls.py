4c4747cadaf6813264ac3f4022451149474538dc
lipadsoft-Engineer <pierreclassique391@gmail.com>
2025-09-15 21:25:35 +0300

    Rearrange url to put formated string url at bottom

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("add", views.add_task, name='add_task'),
     path(f"<str:name>", views.ask, name="ask" ),
-    path("add", views.add_task, name='add_task')
 ]
