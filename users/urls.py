cb4d1f31527fda9fbef1d232a7d2e7176eb28d8e
lipadsoft-Engineer <pierreclassique391@gmail.com>
2025-09-21 10:56:26 +0300

    Add correct url pattern for var

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.users, name="users"),
-    path("<str:name>", views.bonjour, name="bonjour"),
+    path(f"<str:name>/", views.bonjour, name="bonjour"),
     path("new year/", views.is_new_year, name="new_year")
 ]
