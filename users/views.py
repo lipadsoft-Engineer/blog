08bfbe608f7d2f5b17060aa7a4d7fe642b0e5f68
lipadsoft-Engineer <pierreclassique391@gmail.com>
2025-10-07 15:51:30 +0300

    Add serverside validation for create user

--- users/views.py
+++ users/views.py
@@ -13,11 +13,23 @@ roles = ["SuperAdmin"]
 now = datetime.datetime.now()
 
 def users(request):
+    if request.method == 'POST':
+        create_user_form = CreateUser(request.POST)
+        if create_user_form.is_valid():
+            user_name = create_user_form.cleaned_data['user_name'].capitalize()
+            users_list.append(user_name)
+            user_role = create_user_form.cleaned_data['user_role']
+            roles.append(user_role)
+        else:
+            return render(request, "users/users.html", {
+                "create_user": create_user_form
+            })
+        
     zipped_user_data = zip(users_list, roles)
     return render(request, "users/users.html", {
         "zipped_user_data": zipped_user_data,
         "create_user": CreateUser()
-    })
+    })    
 
 def bonjour(request, name):
     return HttpResponse(f"Bonjour, {name.capitalize()}!")
