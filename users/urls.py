4f59c9d7cacd36eeb41580952934d3d7eeb92c74
lipadsoft-Engineer <pierreclassique391@gmail.com>
2025-10-07 14:28:14 +0300

    Refactor hierarchy of urls

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.users, name="users"),
+    path("new year/", views.is_new_year, name="new_year"),
     path(f"<str:name>/", views.bonjour, name="bonjour"),
-    path("new year/", views.is_new_year, name="new_year")
 ]
