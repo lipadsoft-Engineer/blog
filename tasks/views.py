d7867a42c6d76e05d67d3b1490f9dada17560832
lipadsoft-Engineer <pierreclassique391@gmail.com>
2025-10-07 12:47:29 +0300

    Add server side validation

--- tasks/views.py
+++ tasks/views.py
@@ -3,7 +3,7 @@ from django import forms
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="Task")
-    priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
+    priority = forms.IntegerField(label="Priority", min_value=1, max_value=5)
 
 
 tasks = [ "sleep", "pray", "eat"]
@@ -20,6 +20,16 @@ def ask(request, name):
     })
 
 def add_task(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+        else:
+            return render(request, "tasks/add_task.html", {
+                "form":form
+            })
+
     return render(request, "tasks/add_task.html", {
         "form": NewTaskForm()
     } )
