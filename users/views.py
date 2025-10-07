aad722546d6c083b2c8601cb5b34c27a2f67d4c5
lipadsoft-Engineer <pierreclassique391@gmail.com>
2025-10-07 15:15:45 +0300

    Create add user form and pass arg in view

--- users/views.py
+++ users/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django import forms
 import datetime
+
+class CreateUser(forms.Form):
+    user_name = forms.CharField(label='Names')
+    user_role = forms.ChoiceField(choices=[('A', 'Admin'), ('U', 'User')], widget=forms.RadioSelect)
 
 users_list = ["Peter Ngare"]
 roles = ["SuperAdmin"]
@@ -11,6 +16,7 @@ def users(request):
     zipped_user_data = zip(users_list, roles)
     return render(request, "users/users.html", {
         "zipped_user_data": zipped_user_data,
+        "create_user": CreateUser()
     })
 
 def bonjour(request, name):
