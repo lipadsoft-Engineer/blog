a81d2cb6a9ad75c8f8dac92efd932e59f92194e4
lipadsoft-Engineer <pierreclassique391@gmail.com>
2025-10-07 14:37:37 +0300

    Zip user role DS and add it as arg to view.

--- users/views.py
+++ users/views.py
@@ -2,13 +2,16 @@ from django.http import HttpResponse
 from django.shortcuts import render
 import datetime
 
-users = ["Peter"]
+users_list = ["Peter Ngare"]
 roles = ["SuperAdmin"]
 
 now = datetime.datetime.now()
 
 def users(request):
-    return render(request, "users/index.html")
+    zipped_user_data = zip(users_list, roles)
+    return render(request, "users/users.html", {
+        "zipped_user_data": zipped_user_data,
+    })
 
 def bonjour(request, name):
     return HttpResponse(f"Bonjour, {name.capitalize()}!")
