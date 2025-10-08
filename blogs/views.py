4b98fcfda2100deb1de8f6eb59502058dc823d92
lipadsoft-Engineer <pierreclassique391@gmail.com>
2025-10-08 22:51:38 +0300

    Initialize blogs list

--- blogs/views.py
+++ blogs/views.py
@@ -7,7 +7,9 @@ counts = [3]
 # Create your views here.
 def index(request):
     zipped_blogs_data = zip(categories, counts)
-    return render(request, "blogs/index.html")
+    return render(request, "blogs/index.html", {
+        "zipped_blogs_data": zipped_blogs_data
+    })
 
 def greet(request, name):
     return render(request, "blogs/greet.html", { "name": name.capitalize() })
