9cd33f4a97f8d32192527ea16534d126ad7cf94e
lipadsoft-Engineer <pierreclassique391@gmail.com>
2025-10-08 22:43:50 +0300

    Add categories and counts list and zip them

--- blogs/views.py
+++ blogs/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+categories = ['Health']
+counts = [3]
+
 # Create your views here.
 def index(request):
+    zipped_blogs_data = zip(categories, counts)
     return render(request, "blogs/index.html")
 
 def greet(request, name):
