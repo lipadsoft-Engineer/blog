5d6a4d7bafbe772c5f0f8f0673a74e83049f2da3
lipadsoft-Engineer <pierreclassique391@gmail.com>
2025-10-09 23:56:58 +0300

    Add profile url

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.users, name="users"),
     path("new year/", views.is_new_year, name="new_year"),
+    path("profile/", views.profile, name="profile"),
     path(f"<str:name>/", views.bonjour, name="bonjour"),
 ]
