98a75742a979b72fecdbe08a73a8ca581d715765
lipadsoft-Engineer <pierreclassique391@gmail.com>
2025-10-10 02:31:48 +0300

    Add default route

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("", include('blogs.urls')),
     path("admin/", admin.site.urls),
     path("accounts/", include('django.contrib.auth.urls')),
     path("tasks/", include("tasks.urls")),
